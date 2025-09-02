15bd301dea8de0822e52f496e17ebbf30c3c5a30
Bruno Conti <brunocse.5@gmail.com>
2025-09-02 09:52:43 -0300

    type checking for return

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -9,30 +9,32 @@ void interpreter(Node* root, Symbol* ts) {
         exit(EXIT_FAILURE);
     }
 
+    Type expected_return_type = root->info->type;
+    
     Node* main_node = searchNodeByToken(root, MAIN);
     if (main_node == NULL) {
         printf("Error: MAIN function not found\n");
         exit(EXIT_FAILURE);
     }
 
-    executeSentens(main_node->right, ts);
+    executeSentens(main_node->right, ts, expected_return_type);
 }
 
-void executeSentens(Node* sens_node, Symbol* ts) {
+void executeSentens(Node* sens_node, Symbol* ts, Type expected_return_type) {
     if (sens_node == NULL) {
         printf("Error: NULL statements node encountered\n");
         exit(EXIT_FAILURE);
     }
 
     if (sens_node->info->token == SENTENS) {
-        executeSentens(sens_node->left, ts);
-        executeSentens(sens_node->right, ts);
+        executeSentens(sens_node->left, ts, expected_return_type);
+        executeSentens(sens_node->right, ts, expected_return_type);
     } else {
-        executeSen(sens_node, ts);
+        executeSen(sens_node, ts, expected_return_type);
     }
 }
 
-void executeSen(Node* sen_node, Symbol* ts) {
+void executeSen(Node* sen_node, Symbol* ts, Type expected_return_type) {
     if (sen_node == NULL) {
         printf("Error: NULL statement node encountered\n");
         exit(EXIT_FAILURE);
@@ -41,7 +43,7 @@ void executeSen(Node* sen_node, Symbol* ts) {
     if (sen_node->info->token == IGUAL) {
         executeAssign(sen_node, ts);
     } else if (sen_node->info->token == RETURN) {
-        executeRet(sen_node, ts);
+        executeRet(sen_node, ts, expected_return_type);
     } else {
         printf("Error: Unknown statement type\n");
         exit(EXIT_FAILURE);
@@ -77,21 +79,59 @@ void executeAssign(Node* assign_node, Symbol* ts) {
     }
 }
 
-void executeRet(Node* ret_node, Symbol* ts) {
+void executeRet(Node* ret_node, Symbol* ts, Type expected_return_type) {
     if (ret_node == NULL) {
         printf("Error: NULL return node encountered\n");
         exit(EXIT_FAILURE);
     }
 
-    if (ret_node->left != NULL) {
-        evaluateExp(ret_node->left, ts);
-        if (ret_node->left->info->type == INTEGER) {
-            printf("Return value: %d\n", ret_node->left->info->i_value);
-        } else if (ret_node->left->info->type == BOOLEAN) {
-            printf("Return value: %s\n", ret_node->left->info->b_value ? "true" : "false");
+    if (ret_node->left == NULL) {
+        if (expected_return_type != TYPE_VOID) {
+            printf("Error: Function expects return type but return statement has no value\n");
+            exit(EXIT_FAILURE);
         }
-    } else {
         printf("Return statement with no value\n");
+        return;
+    }
+
+    evaluateExp(ret_node->left, ts);
+    Type actual_return_type = ret_node->left->info->type;
+
+    if (expected_return_type == TYPE_VOID) {
+        printf("Error: void function cannot return a value\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (actual_return_type != expected_return_type) {
+        printf("Error: Return type mismatch. Expected ");
+        printType(expected_return_type);
+        printf(", but got ");
+        printType(actual_return_type);
+        printf("\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (actual_return_type == INTEGER) {
+        printf("Return value: %d\n", ret_node->left->info->i_value);
+    } else if (actual_return_type == BOOLEAN) {
+        printf("Return value: %s\n", ret_node->left->info->b_value ? "true" : "false");
+    }
+}
+
+void printType(Type type) {
+    switch (type) {
+        case INTEGER:
+            printf("int");
+            break;
+        case BOOLEAN:
+            printf("bool");
+            break;
+        case TYPE_VOID:
+            printf("void");
+            break;
+        default:
+            printf("unknown");
+            break;
     }
 }
 
--- interpreter/interpreter.h
+++ interpreter/interpreter.h
@@ -5,10 +5,11 @@
 #include "../table_of_symbols/table_symbols.h"
 
 void interpreter(Node* root, Symbol* ts);
-void evaluateExp(Node* exp_node, Symbol* ts);
-void executeSentens(Node* sens_node, Symbol* ts);
-void executeSen(Node* sen_node, Symbol* ts);
+void executeSentens(Node* sens_node, Symbol* ts, Type expected_return_type);
+void executeSen(Node* sen_node, Symbol* ts, Type expected_return_type);
 void executeAssign(Node* assign_node, Symbol* ts);
-void executeRet(Node* return_node, Symbol* ts);
+void executeRet(Node* ret_node, Symbol* ts, Type expected_return_type);
+void evaluateExp(Node* exp_node, Symbol* ts);
+void printType(Type type);
 
 #endif
